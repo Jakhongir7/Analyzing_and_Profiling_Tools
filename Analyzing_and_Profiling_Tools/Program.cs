2e146b3a99edf3123f50b735f977e04aff94c96e
Jakhongir <Jakhongir_Bakhodirov@epam.com>
2024-05-08 13:20:49 +0500

    Task 1

--- Analyzing_and_Profiling_Tools/Program.cs
+++ Analyzing_and_Profiling_Tools/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System.Security.Cryptography;
 
-namespace YourNamespace
+namespace Analyzing_and_Profiling_Tools
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            string hashedPassword = GeneratePasswordHashUsingSalt("SomeText", new byte[16]);
-            
+            string password = "SomePassword";
+            byte[] salt = new byte[16];
+
+            string hashedPassword = GeneratePasswordHashUsingSalt(password, salt);
+
             Console.WriteLine("Hashed Password:" + hashedPassword);
-            Console.ReadKey();
         }
 
         public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
         {
-
             var iterate = 10000;
             var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
             byte[] hash = pbkdf2.GetBytes(20);
 
-            byte[] hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
+            // Combine salt and hash into a single byte array
+            byte[] hashBytes = new byte[salt.Length + hash.Length];
 
-            var passwordHash = Convert.ToBase64String(hashBytes);
+            // Replaced the Array.Copy calls with Buffer.BlockCopy, which is generally more efficient for copying large byte arrays.
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
 
-            return passwordHash;
-
+            return Convert.ToBase64String(hashBytes);
         }
     }
 }
